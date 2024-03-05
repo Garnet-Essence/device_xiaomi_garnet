e6b13718c96e883cd4c28a695372297d4430f466
chrisw444 <wandersonrodriguesf1@gmail.com>
2024-03-05 18:02:54 -0300

    garnet: Build android.hardware.thermal@2.0 vendor variant from source
    *  CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.qti.hardware.limits@1.0-service": library "android.hardware.thermal@2.0.so" not found: needed by main executable

--- device.mk
+++ device.mk
@@ -535,8 +535,9 @@ PRODUCT_COPY_FILES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0.vendor \
     android.hardware.thermal-service.qti
-
+ 
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
