4b7f2763e8e3042547ea99bb5b47e2a951a90668
Michael Bestas <mkbestas@lineageos.org>
2024-11-25 15:19:52 -0300

    garnet: Shim libwvhidl with libcrypto_shim
    The following change
    google/boringssl@e202e51
    removed symbols that our libs require.
    
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29

--- extract-files.sh
+++ extract-files.sh
@@ -76,6 +76,10 @@ function blob_fixup() {
         vendor/lib64/vendor.libdpmframework.so)
             "${PATCHELF_0_17_2}" --add-needed "libhidlbase_shim.so" "${2}"
             ;;      
+        vendor/lib64/libwvhidl.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+            ;;              
     esac
 }
 
